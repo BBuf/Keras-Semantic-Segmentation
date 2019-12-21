2a9b4c75c892270d4b5ad76505be7908b35ea187
BBuf <1182563586@qq.com>
2019-12-21 12:42:36 +0800

    Init

---
+++ data.py
@@ -0,0 +1,43 @@
+#coding=utf-8
+import numpy as np
+import cv2
+import glob
+import itertools
+
+def getImage(path, width, height):
+    img = cv2.imread(path)
+    # resize到网络输入大小
+    img = cv2.resize(img, dsize=(width, height))
+    return img
+
+def getLable(path, n_classes, width, height):
+    seg_labels = np.zeros((height, width, n_classes))
+    img = cv2.imread(path, 0)
+    img = cv2.resize(img, (width, height))
+    for c in range(n_classes):
+        seg_labels[:, :, c] = (img == c).astype(int)
+    seg_labels = np.reshape(seg_labels, (width * height, n_classes))
+    return seg_labels
+
+def imageSegmentationGenerator(images_path, segs_path, batch_size, n_classes,
+                               input_height, input_width, output_height,
+                               output_width):
+    assert images_path[-1] == '/'
+    assert segs_path[-1] == '/'
+    images = glob.glob(images_path + "*.jpg") + glob.glob(images_path + "*.png") + glob.glob(images_path + "*.jpeg")
+    images.sort()
+    segmentations = glob.glob(segs_path + "*.jpg") + glob.glob(segs_path + "*.png") + glob.glob(segs_path + "*.jpeg")
+    segmentations.sort()
+    assert len(images) == len(segmentations)
+    zipped = itertools.cycle(zip(images, segmentations))
+    while True:
+        X = []
+        Y = []
+        for _ in range(batch_size):
+            im, seg = next(zipped)
+            X.append(getImage(im, input_width, input_height))
+            Y.append(getLable(seg, n_classes, output_width, output_height))
+        yield np.array(X), np.array(Y)
+
+
+
--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 #coding=utf-8
+import keras
 import argparse
+import data
+import Models
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, CSVLogger, ModelCheckpoint
 from Models import ENet
 from Models import FCN8
 from Models import FCN32
@@ -20,45 +24,51 @@ from Models import MobileNetUnet
 from Models import MobileNetFCN8
 from Models import MobileNetFCN32
 from Models import MobileNetSegnet
-import Models , LoadBatches
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--save_weights_path", type = str)
-parser.add_argument("--train_images", type = str)
-parser.add_argument("--train_annotations", type = str)
-parser.add_argument("--n_classes", type=int)
+parser.add_argument("--train_images", type = str, default="data/train_image")
+parser.add_argument("--train_annotations", type = str, default="data/train_label")
+parser.add_argument("--n_classes", type=int, default=2)
 parser.add_argument("--input_height", type=int, default = 512)
 parser.add_argument("--input_width", type=int, default = 512)
-parser.add_argument('--validate', action='store_false')
-parser.add_argument("--val_images", type = str, default = "")
-parser.add_argument("--val_annotations", type = str, default = "")
-parser.add_argument("--epochs", type = int, default = 5)
-parser.add_argument("--batch_size", type = int, default = 2)
+parser.add_argument('--validate', type=bool, default=True)
+parser.add_argument("--val_images", type = str, default = "data/val_image")
+parser.add_argument("--val_annotations", type = str, default = "data/val_label")
+parser.add_argument("--epochs", type = int, default = 20)
+parser.add_argument("--train_batch_size", type = int, default = 4)
 parser.add_argument("--val_batch_size", type = int, default = 2)
-parser.add_argument("--load_weights", type = str, default = "")
-parser.add_argument("--model_name", type = str, default = "")
-parser.add_argument("--optimizer_name", type = str, default = "adadelta")
-
+parser.add_argument("--train_save_path", type = str, default = "weights/unet")
+parser.add_argument("--resume", type=str, default="")
+parser.add_argument("--model_name", type = str, default = "unet")
+parser.add_argument("--optimizer_name", type=str, default="sgd")
 
 args = parser.parse_args()
 
-train_images_path = args.train_images
-train_segs_path = args.train_annotations
+# 再定义一些keras回调函数需要的参数
+# patience：没有提升的轮次，即训练过程中最多容忍多少次没有提升
+patience = 10
+# log_file_path：日志保存的路径
+log_file_path = 'weights/log.csv'
+
+train_images = args.train_images
+train_segs = args.train_annotations
 train_batch_size = args.batch_size
+
+validate = args.validate
 n_classes = args.n_classes
 input_height = args.input_height
 input_width = args.input_width
-validate = args.validate
-save_weights_path = args.save_weights_path
-epochs = args.epochs
-load_weights = args.load_weights
 
-optimizer_name = args.optimizer_name
+train_save_path = args.train_save_path
+epochs = args.epochs
+load_weights = args.resume
+
 model_name = args.model_name
+optimizer_name = args.optimizer_name
 
 if validate:
-	val_images_path = args.val_images
-	val_segs_path = args.val_annotations
+	val_images = args.val_images
+	val_segs = args.val_annotations
 	val_batch_size = args.val_batch_size
 
 modelFns = {'enet':Models.ENet.ENet,
@@ -84,35 +94,34 @@ modelFns = {'enet':Models.ENet.ENet,
 			'mobilenet_segnet':Models.MobileNetSegnet.MobileNetSegnet
 			}
 modelFN = modelFns[model_name]
+model = modelFN(n_classes, input_height=input_height, input_width=input_width)
 
-m = modelFN(n_classes, input_height=input_height, input_width=input_width)
-m.compile(loss='categorical_crossentropy',
-      optimizer= optimizer_name,
-      metrics=['accuracy'])
+# 模型回调函数
+early_stop = EarlyStopping('loss', min_delta=0.1, patience=patience, verbose=1)
+reduce_lr = ReduceLROnPlateau('loss', factor=0.1, patience=int(patience/2), verbose=1)
+csv_logger = CSVLogger(log_file_path, append=False)
+model_names = train_save_path + '.{epoch:02d}-{acc:2f}.hdf5'
+model_checkpoint = ModelCheckpoint(model_names, monitor='loss', save_best_only=True, save_weights_only=False)
+call_backs = [model_checkpoint, csv_logger, early_stop, reduce_lr]
+
+model.compile(loss='categorical_crossentropy', optimizer= optimizer_name, metrics=['accuracy'])
 
 
 if len(load_weights) > 0:
-	m.load_weights(load_weights)
+	model.load(load_weights)
+print("Model output shape : ",  model.output_shape)
 
+output_height = model.outputHeight
+output_width = model.outputWidth
 
-print("Model output shape : ",  m.output_shape)
-
-output_height = m.outputHeight
-output_width = m.outputWidth
-
-G = LoadBatches.imageSegmentationGenerator(train_images_path, train_segs_path,  train_batch_size,  n_classes, input_height, input_width, output_height, output_width)
-
+train_ge = data.imageSegmentationGenerator(train_images, train_segs,  train_batch_size,
+									 n_classes, input_height, input_width, output_height, output_width)
 
 if validate:
-	G2 = LoadBatches.imageSegmentationGenerator(val_images_path, val_segs_path,  val_batch_size,  n_classes, input_height, input_width, output_height, output_width)
+	val_ge = data.imageSegmentationGenerator(val_images, val_segs,  val_batch_size,
+											 n_classes, input_height, input_width, output_height, output_width)
 
 if not validate:
-	for ep in range(epochs):
-		m.fit_generator(G, 100, epochs=1)
-		m.save_weights(save_weights_path + "." + str(ep))
-		m.save(save_weights_path + ".model." + str(ep))
+	model.fit_generator(train_ge, epochs=100, call_backs=call_backs, verbose=1, shuffle=True)
 else:
-	for ep in range(epochs):
-		m.fit_generator(G, 100, validation_data=G2, validation_steps=20,  epochs=1)
-		m.save_weights(save_weights_path + "." + str(ep))
-		m.save(save_weights_path + ".model." + str(ep))
+	model.fit_generator(train_ge, epochs=100, call_backs=call_backs, verbose=1, shuffle=True, validation_steps=5)
