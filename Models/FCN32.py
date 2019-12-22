b1bde3d87e25b13c6ca4618d7cd80c422fc4a4bc
BBuf <1182563586@qq.com>
2019-12-22 22:36:45 +0800

    Add Init

--- Models/FCN32.py
+++ Models/FCN32.py
@@ -3,34 +3,34 @@ from keras.models import *
 from keras.layers import *
 
 
-def FCN32(n_classes,  input_height=224, input_width=224):
+def FCN32(nClasses, input_height=224, input_width=224):
 
 	img_input = Input(shape=(input_height, input_width, 3))
 
-	x = Conv2D(16, (3, 3), activation='relu', padding='same')(img_input)
+	x = Conv2D(32, (3, 3), activation='relu', padding='same')(img_input)
 	x = BatchNormalization()(x)
 	x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 	f1 = x
 	# 112 x 112
-	x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+	x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 	x = BatchNormalization()(x)
 	x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 	f2 = x
 
-	# 64 x 64
+	# 56 x 56
 	x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 	x = BatchNormalization()(x)
 	x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 	f3 = x
- 
-	# 32 x 32
+
+	# 28 x 28
 	x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
 	x = BatchNormalization()(x)
 	x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 	f4 = x
 
-	# 16 x 16
-	x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+	# 14 x 14
+	x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
 	x = BatchNormalization()(x)
 	x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 	f5 = x
@@ -38,19 +38,19 @@ def FCN32(n_classes,  input_height=224, input_width=224):
 
 	o = f5
 
-	o = (Conv2D(256, (7, 7), activation='relu', padding='same'))(o)
-	x = BatchNormalization()(x)
+	o = (Conv2D(128, (7, 7), activation='relu', padding='same'))(o)
+	o = BatchNormalization()(o)
 
-	o = (Conv2D(n_classes,  (1, 1)))(o)
-	o = Conv2DTranspose(n_classes, kernel_size=(64, 64),  strides=(32, 32))(o)
+	o = (Conv2D(nClasses, (1, 1), activation='relu'))(o)
+	o = Conv2DTranspose(nClasses, kernel_size=(32, 32),  strides=(32, 32), padding="valid")(o)
 	o_shape = Model(img_input, o).output_shape
 	
 	outputHeight = o_shape[1]
 	outputWidth = o_shape[2]
 
-	o = (Reshape((outputHeight*outputWidth, nClasses)))(o)
+	o = (Reshape((outputHeight * outputWidth, nClasses)))(o)
 	o = (Activation('softmax'))(o)
-	model = Model(img_input, o )
+	model = Model(img_input, o)
 	model.outputWidth = outputWidth
 	model.outputHeight = outputHeight
 
--- Models/Segnet.py
+++ Models/Segnet.py
@@ -2,60 +2,72 @@
 from keras.models import *
 from keras.layers import *
 
-def Segnet(n_classes, input_height=512, input_width=512):
-	img_input = Input(shape=(input_height, input_width, 3))
-	#Encoder
-	x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
-	x = (BatchNormalization())(x)
-	x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+def Segnet(nClasses, input_height=224, input_width=224):
+    inputs = Input(shape=(input_height, input_width, 3))
+    #Encoder
+    # 224x224
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    # 112x112
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    # 56x56
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # 28x28
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    # 14x14
 
-	x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-	x = (BatchNormalization())(x)
-	x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    # decode
+    up7 = UpSampling2D(size=(2, 2))(pool4)
+    #up7 = concatenate([up7, conv4], axis=-1)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
 
-	x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-	x = (BatchNormalization())(x)
-	x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    up8 = UpSampling2D(size=(2, 2))(conv7)
+    #up8 = concatenate([up8, conv3], axis=-1)
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv8)
+    conv8 = BatchNormalization()(conv8)
 
-	x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-	x = (BatchNormalization())(x)
-	x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    up9 = UpSampling2D(size=(2, 2))(conv8)
+    #up9 = concatenate([up9, conv2], axis=-1)
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv9)
+    conv9 = BatchNormalization()(conv9)
 
-	#decoder
-	o = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
-	o = (BatchNormalization())(o)
+    up10 = UpSampling2D(size=(2, 2))(conv9)
+    #up10 = concatenate([up10, conv1], axis=-1)
+    conv10 = Conv2D(32, (3, 3), activation='relu', padding='same')(up10)
+    conv10 = BatchNormalization()(conv10)
+    conv10 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv10)
+    conv10 = BatchNormalization()(conv10)
+    outputHeight = Model(inputs, conv10).output_shape[1]
+    outputWidth = Model(inputs, conv10).output_shape[2]
+    conv11 = Conv2D(nClasses, (1, 1), padding='same')(conv10)
+    conv11 = (Reshape((outputHeight*outputWidth, nClasses)))(conv11)
+    conv11 = Activation('softmax')(conv11)
 
-	o = (UpSampling2D((2, 2)))(o)
-	o = (ZeroPadding2D((1, 1)))(o)
-	o = (Conv2D(256, (3, 3), padding='valid'))(o)
-	o = (BatchNormalization())(o)
-
-	o = (UpSampling2D((2, 2)))(o)
-	o = (ZeroPadding2D((1, 1)))(o)
-	o = (Conv2D(128, (3, 3), padding='valid'))(o)
-	o = (BatchNormalization())(o)
-
-	o = (UpSampling2D((2, 2)))(o)
-	o = (ZeroPadding2D((1, 1)))(o)
-	o = (Conv2D(64, (3, 3), padding='valid'))(o)
-	o = (BatchNormalization())(o)
-
-	o = (UpSampling2D((2, 2)))(o)
-	o = (ZeroPadding2D((1, 1)))(o)
-	o = (Conv2D(32, (3, 3), padding='valid'))(o)
-	o = (BatchNormalization())(o)
+    model = Model(inputs, conv11)
+    model.outputWidth = outputWidth
+    model.outputHeight = outputHeight
 	
-	o = Conv2D(n_classes, (3, 3), padding='same')(o)
-	o_shape = Model(img_input, o).output_shape
-	outputHeight = o_shape[1]
-	outputWidth = o_shape[2]
-	print(outputHeight)
-	print(outputWidth)
-	o = Reshape((n_classes, input_height * input_width))(o)
-	o = Permute((2, 1))(o)
-	model = Model(img_input, o)
-	model.outputWidth = outputWidth
-	model.outputHeight = outputHeight
-	
-	return model
+    return model
 
