bd3c0547bc0efcdd5f2f338428564b891961b18f
BBuf <1182563586@qq.com>
2019-12-31 10:38:52 +0800

    Add FCN8

--- Models/FCN8.py
+++ Models/FCN8.py
@@ -3,22 +3,50 @@ from keras.models import *
 from keras.layers import *
 import os
 
+# crop o1 wrt o2
+def crop(o1, o2, i):
+    o_shape2 = Model(i, o2).output_shape
+
+
+    output_height2 = o_shape2[1]
+    output_width2 = o_shape2[2]
+
+    o_shape1 = Model(i, o1).output_shape
+
+    output_height1 = o_shape1[1]
+    output_width1 = o_shape1[2]
+
+    cx = abs(output_width1 - output_width2)
+    cy = abs(output_height2 - output_height1)
+
+    if output_width1 > output_width2:
+        o1 = Cropping2D(cropping=((0, 0),  (0, cx)))(o1)
+    else:
+        o2 = Cropping2D(cropping=((0, 0),  (0, cx)))(o2)
+
+    if output_height1 > output_height2:
+        o1 = Cropping2D(cropping=((0, cy),  (0, 0)))(o1)
+    else:
+        o2 = Cropping2D(cropping=((0, cy),  (0, 0)))(o2)
+
+    return o1, o2
+
 def FCN8(nClasses, input_height=224, input_width=224):
 
 	img_input = Input(shape=(input_height, input_width, 3))
 
-	x = Conv2D(32, (3, 3), activation='relu', padding='same')(img_input)
+	x = Conv2D(64, (3, 3), activation='relu', padding='same')(img_input)
 	x = BatchNormalization()(x)
 	x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 	f1 = x
 	# 112 x 112
-	x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+	x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
 	x = BatchNormalization()(x)
 	x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 	f2 = x
 
 	# 56 x 56
-	x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+	x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
 	x = BatchNormalization()(x)
 	x = MaxPooling2D((2, 2), strides=(2, 2))(x)
 	f3 = x
@@ -43,26 +71,26 @@ def FCN8(nClasses, input_height=224, input_width=224):
 
 
 	o = (Conv2D(nClasses, (1, 1)))(o)
-	# W = (N - 1) * S - 2P + F = 6 * 2 - 0 + 2 = 14
-	o = Conv2DTranspose(nClasses, kernel_size=(2, 2), strides=(2, 2), padding="valid")(o)
-	# 14 x 14
+	# W = (N - 1) * S - 2P + F = 6 * 2 - 0 + 4 = 16
+	o = Conv2DTranspose(nClasses, kernel_size=(4, 4), strides=(2, 2), padding="valid")(o)
+
 
 	o2 = f4
 	o2 = (Conv2D(nClasses, (1, 1)))(o2)
 	
-	# (14 x 14) (14 x 14)
-
+	
+	o, o2 = crop(o, o2, img_input)
 	o = Add()([o, o2])
         # W = (N - 1) * S - 2P + F = 13 * 2 - 0 + 2 = 28
-	o = Conv2DTranspose(nClasses, kernel_size=(2, 2),  strides=(2, 2), padding="valid")(o)
+	o = Conv2DTranspose(nClasses, kernel_size=(4, 4),  strides=(2, 2), padding="valid")(o)
 	o2 = f3 
 	o2 = (Conv2D(nClasses,  (1, 1)))(o2)
-	# (28 x 28) (28 x 28)
+        
+	o2, o = crop(o2, o, img_input)
 	o = Add()([o2, o])
 
-	# 224 x 224
 	# W = (N - 1) * S - 2P + F = 27 * 8 + 8 = 224
-	o = Conv2DTranspose(nClasses , kernel_size=(8,8),  strides=(8,8), padding="valid")(o)
+	o = Conv2DTranspose(nClasses , kernel_size=(16,16),  strides=(8,8), padding="valid")(o)
 	
 	o_shape = Model(img_input, o).output_shape
 	
