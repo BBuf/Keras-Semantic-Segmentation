ca08f1a4b6245789976a09e919a1bb4c664d13ce
BBuf <1182563586@qq.com>
2020-03-14 10:15:58 +0800

    Add Unet.py

--- Models/Unet.py
+++ Models/Unet.py
@@ -11,14 +11,14 @@ from keras.optimizers import *
 
 def Unet(nClasses, input_height=224, input_width=224):
     inputs = Input(shape=(input_height, input_width, 3))
-    conv1 = Conv2D(64,
+    conv1 = Conv2D(16,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(inputs)
     conv1 = BatchNormalization()(conv1)
 
-    conv1 = Conv2D(64,
+    conv1 = Conv2D(16,
                    3,
                    activation='relu',
                    padding='same',
@@ -26,14 +26,14 @@ def Unet(nClasses, input_height=224, input_width=224):
     conv1 = BatchNormalization()(conv1)
 
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(128,
+    conv2 = Conv2D(32,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(pool1)
     conv2 = BatchNormalization()(conv2)
 
-    conv2 = Conv2D(128,
+    conv2 = Conv2D(32,
                    3,
                    activation='relu',
                    padding='same',
@@ -41,14 +41,14 @@ def Unet(nClasses, input_height=224, input_width=224):
     conv2 = BatchNormalization()(conv2)
 
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(256,
+    conv3 = Conv2D(64,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(pool2)
     conv3 = BatchNormalization()(conv3)
 
-    conv3 = Conv2D(256,
+    conv3 = Conv2D(64,
                    3,
                    activation='relu',
                    padding='same',
@@ -56,14 +56,14 @@ def Unet(nClasses, input_height=224, input_width=224):
     conv3 = BatchNormalization()(conv3)
 
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(512,
+    conv4 = Conv2D(128,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(pool3)
     conv4 = BatchNormalization()(conv4)
 
-    conv4 = Conv2D(512,
+    conv4 = Conv2D(128,
                    3,
                    activation='relu',
                    padding='same',
@@ -72,20 +72,20 @@ def Unet(nClasses, input_height=224, input_width=224):
 
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-    conv5 = Conv2D(1024,
+    conv5 = Conv2D(256,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(pool4)
     conv5 = BatchNormalization()(conv5)
-    conv5 = Conv2D(1024,
+    conv5 = Conv2D(256,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(conv5)
     conv5 = BatchNormalization()(conv5)
 
-    up6 = Conv2D(512,
+    up6 = Conv2D(128,
                  2,
                  activation='relu',
                  padding='same',
@@ -94,21 +94,21 @@ def Unet(nClasses, input_height=224, input_width=224):
     up6 = BatchNormalization()(up6)
 
     merge6 = concatenate([conv4, up6], axis=3)
-    conv6 = Conv2D(512,
+    conv6 = Conv2D(128,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(merge6)
     conv6 = BatchNormalization()(conv6)
 
-    conv6 = Conv2D(512,
+    conv6 = Conv2D(128,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(conv6)
     conv6 = BatchNormalization()(conv6)
 
-    up7 = Conv2D(256,
+    up7 = Conv2D(64,
                  2,
                  activation='relu',
                  padding='same',
@@ -117,21 +117,21 @@ def Unet(nClasses, input_height=224, input_width=224):
     up7 = BatchNormalization()(up7)
 
     merge7 = concatenate([conv3, up7], axis=3)
-    conv7 = Conv2D(256,
+    conv7 = Conv2D(64,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(merge7)
     conv7 = BatchNormalization()(conv7)
 
-    conv7 = Conv2D(256,
+    conv7 = Conv2D(64,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(conv7)
     conv7 = BatchNormalization()(conv7)
 
-    up8 = Conv2D(128,
+    up8 = Conv2D(32,
                  2,
                  activation='relu',
                  padding='same',
@@ -140,21 +140,21 @@ def Unet(nClasses, input_height=224, input_width=224):
     up8 = BatchNormalization()(up8)
 
     merge8 = concatenate([conv2, up8], axis=3)
-    conv8 = Conv2D(128,
+    conv8 = Conv2D(32,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(merge8)
     conv8 = BatchNormalization()(conv8)
 
-    conv8 = Conv2D(128,
+    conv8 = Conv2D(32,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(conv8)
     conv8 = BatchNormalization()(conv8)
 
-    up9 = Conv2D(64,
+    up9 = Conv2D(16,
                  2,
                  activation='relu',
                  padding='same',
@@ -163,21 +163,14 @@ def Unet(nClasses, input_height=224, input_width=224):
     up9 = BatchNormalization()(up9)
 
     merge9 = concatenate([conv1, up9], axis=3)
-    conv9 = Conv2D(64,
+    conv9 = Conv2D(16,
                    3,
                    activation='relu',
                    padding='same',
                    kernel_initializer='he_normal')(merge9)
     conv9 = BatchNormalization()(conv9)
 
-    conv9 = Conv2D(64,
-                   3,
-                   activation='relu',
-                   padding='same',
-                   kernel_initializer='he_normal')(conv9)
-    conv9 = BatchNormalization()(conv9)
-
-    conv9 = Conv2D(2,
+    conv9 = Conv2D(16,
                    3,
                    activation='relu',
                    padding='same',
