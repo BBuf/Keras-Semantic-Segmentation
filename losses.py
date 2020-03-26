58fe066eaeea75b4ffe1a3e6f04754124b9e7787
zxyyyyyyyyyyyyyyy <35585791+BBuf@users.noreply.github.com>
2020-03-26 12:00:09 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,14 +1,23 @@
 #coding=utf-8
 #https://blog.csdn.net/m0_37477175/article/details/83004746
 from keras import backend as K
+import tensorflow as tf
 
-# model.compile(optimizer=optimizer, loss=[focal_loss(alpha=.25, gamma=2)])
+# 针对二分类的二元交叉熵Loss
+def binary_crossentropy_loss(y_true, y_pred):
+    pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+    pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+    return -K.mean(K.log(pt_1)) - K.mean(K.log(1. - pt_0))
+
+# 针对二分类的Focal Loss
 def focal_loss(gamma=2., alpha=.25):
-    def focal_loss_(y_true, y_pred):
-        pt_1 = K.where(K.equal(y_true, 1), y_pred, K.ones_like(y_pred))
-        pt_0 = K.where(K.equal(y_true, 0), y_pred, K.zeros_like(y_pred))
-        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum((1 - alpha))
-    return focal_loss_
+	def focal_loss_fixed(y_true, y_pred):
+		pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+		pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+		return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+	return focal_loss_fixed
+
+
 
 
 
