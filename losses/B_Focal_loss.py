64ed347b81724da0b6299f3b2f8d6b15462b33be
BBuf <1182563586@qq.com>
2020-03-26 15:42:45 +0800

    Add B_Focal_loss.py

---
+++ losses/B_Focal_loss.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+from keras import backend as K
+import tensorflow as tf
+
+
+def focal_loss(gamma=2., alpha=.25):
+	def focal_loss_fixed(y_true, y_pred):
+		pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+		pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+		return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+	return focal_loss_fixed
