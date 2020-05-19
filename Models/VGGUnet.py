a516e015c876feacc5669aa3ed9ca65ba23ac317
BBuf <1182563586@qq.com>
2020-05-19 23:10:57 +0800

    Add VGGUNet

---
+++ Models/VGGUnet.py
@@ -0,0 +1,148 @@
+# coding=utf-8
+from keras.layers import *
+from keras.models import *
+from keras.optimizers import *
+
+
+def VGGUnet(nClasses, input_height=224, input_width=224, vgg_weight_path=None):
+    inputs = Input(shape=(input_height, input_width, 3))
+    # Block 1
+    x = Conv2D(64, (3, 3), padding='same', name='block1_conv1')(inputs)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(64, (3, 3), padding='same', name='block1_conv2')(x)
+    x = BatchNormalization()(x)
+    block_1_out = Activation('relu')(x)
+
+    x = MaxPooling2D()(block_1_out)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), padding='same', name='block2_conv1')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(128, (3, 3), padding='same', name='block2_conv2')(x)
+    x = BatchNormalization()(x)
+    block_2_out = Activation('relu')(x)
+
+    x = MaxPooling2D()(block_2_out)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), padding='same', name='block3_conv1')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(256, (3, 3), padding='same', name='block3_conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(256, (3, 3), padding='same', name='block3_conv3')(x)
+    x = BatchNormalization()(x)
+    block_3_out = Activation('relu')(x)
+
+    x = MaxPooling2D()(block_3_out)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), padding='same', name='block4_conv1')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(512, (3, 3), padding='same', name='block4_conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(512, (3, 3), padding='same', name='block4_conv3')(x)
+    x = BatchNormalization()(x)
+    block_4_out = Activation('relu')(x)
+
+    x = MaxPooling2D()(block_4_out)
+
+    # Block 5
+    x = Conv2D(512, (3, 3), padding='same', name='block5_conv1')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(512, (3, 3), padding='same', name='block5_conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(512, (3, 3), padding='same', name='block5_conv3')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    for_pretrained_weight = MaxPooling2D()(x)
+
+    # Load pretrained weights.
+    if vgg_weight_path is not None:
+        vgg16 = Model(inputs, for_pretrained_weight)
+        vgg16.load_weights(vgg_weight_path, by_name=True)
+
+    # UP 1
+    x = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = concatenate([x, block_4_out])
+    x = Conv2D(512, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(512, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # UP 2
+    x = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = concatenate([x, block_3_out])
+    x = Conv2D(256, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(256, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # UP 3
+    x = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = concatenate([x, block_2_out])
+    x = Conv2D(128, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(128, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # UP 4
+    x = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = concatenate([x, block_1_out])
+    x = Conv2D(64, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(64, (3, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+
+    outputHeight = Model(inputs, x).output_shape[1]
+    outputWidth = Model(inputs, x).output_shape[2]
+
+    out = (Reshape((outputHeight * outputWidth, nClasses)))(x)
+    out = Activation('softmax')(out)
+
+    model = Model(input=inputs, output=out)
+    model.outputHeight = outputHeight
+    model.outputWidth = outputWidth
+
+    return model
