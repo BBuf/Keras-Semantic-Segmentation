0aac3bbc6b12c37f56185d387a719c64276ed5cd
BBuf <1182563586@qq.com>
2020-05-26 23:01:15 +0800

    Add Unet_Family

--- Models/UNet_Family.py
+++ Models/UNet_Family.py
@@ -271,6 +271,127 @@ def AttUNet(nClasses, input_height=224, input_width=224):
     return model
 
 
+def R2AttUNet(nClasses, input_height=224, input_width=224):
+     """
+    Residual Recuurent Block with attention Unet
+    Implementation : https://github.com/LeeJunHyun/Image_Segmentation
+    """
+    inputs = Input(shape=(input_height, input_width, 3))
+    t = 2
+    n1 = 32
+    filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+
+    e1 = RRCNN_block(filters[0], t=t)(inputs)
+
+    e2 = MaxPooling2D(strides=2)(e1)
+    e2 = RRCNN_block(filters[1], t=t)(e2)
+
+    e3 = MaxPooling2D(strides=2)(e2)
+    e3 = RRCNN_block(filters[2], t=t)(e3)
+
+    e4 = MaxPooling2D(strides=2)(e3)
+    e4 = RRCNN_block(filters[3], t=t)(e4)
+
+    e5 = MaxPooling2D(strides=2)(e4)
+    e5 = RRCNN_block(filters[4], t=t)(e5)
+
+    d5 = up_conv(filters[3])(e5)
+    x4 =  Attention_block(filters[3])([d5, e4])
+    d5 = Concatenate()([x4, d5])
+    d5 = conv_block(filters[3])(d5)
+
+    d4 = up_conv(filters[2])(d5)
+    x3 =  Attention_block(filters[2])([d4, e3])
+    d4 = Concatenate()([x3, d4])
+    d4 = conv_block(filters[2])(d4)
+
+    d3 = up_conv(filters[1])(d4)
+    x2 =  Attention_block(filters[1])([d3, e2])
+    d3 = Concatenate()([x2, d3])
+    d3 = conv_block(filters[1])(d3)
+
+    d2 = up_conv(filters[0])(d3)
+    x1 =  Attention_block(filters[0])([d2, e1])
+    d2 = Concatenate()([x1, d2])
+    d2 = conv_block(filters[0])(d2)
+
+    o = Conv2D(nClasses, (3, 3), padding='same')(d2)
+
+    outputHeight = Model(inputs, o).output_shape[1]
+    outputWidth = Model(inputs, o).output_shape[2]
+
+    out = (Reshape((outputHeight * outputWidth, nClasses)))(o)
+    out = Activation('softmax')(out)
+
+    model = Model(input=inputs, output=out)
+    model.outputHeight = outputHeight
+    model.outputWidth = outputWidth
+
+    return model
 
 
+class conv_block_nested(Model):
 
+    def __init__(self, mid_ch, out_ch):
+        super(conv_block_nested, self).__init__()
+        self.activation = Activation('relu')
+        self.conv1 = Conv2D(mid_ch, kernel_size=3, padding='same')
+        self.bn1 = BatchNormalization()
+        self.conv2 = Conv2D(out_ch, kernel_size=3, padding='same')
+        self.bn2 = BatchNormalization()
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        output = self.activation(x)
+
+        return output
+
+class NestedUNet(nClasses, input_height=224, input_width=224):
+    """
+    Implementation of this paper:
+    https://arxiv.org/pdf/1807.10165.pdf
+    """
+
+    inputs = Input(shape=(input_height, input_width, 3))
+    t = 2
+    n1 = 32
+    filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+
+    x0_0 = conv_block_nested(filters[0], filters[0])(x)
+
+    x1_0 = conv_block_nested(filters[1], filters[1])(MaxPooling2D(strides=2)(x0_0))
+    x0_1 = conv_block_nested(filters[0], filters[0])(Concatenate()([x0_0, UpSampling2D()(x1_0)]))
+
+    x2_0 = conv_block_nested(filters[2], filters[2])(MaxPooling2D(strides=2)(x1_0))
+    x1_1 = conv_block_nested(filters[1], filters[1])(Concatenate()([x1_0, UpSampling2D()(x2_0)]))
+    x0_2 = conv_block_nested(filters[0], filters[0])(Concatenate()([x0_0, x0_1, UpSampling2D()(x1_1)]))
+
+    x3_0 = conv_block_nested(filters[3], filters[3])(MaxPooling2D(strides=2)(x2_0))
+    x2_1 = conv_block_nested(filters[2], filters[2])(Concatenate()([x2_0, UpSampling2D()(x3_0)]))
+    x1_2 = conv_block_nested(filters[1], filters[1])(Concatenate()([x1_0, x1_1, UpSampling2D()(x2_1)]))
+    x0_3 = conv_block_nested(filters[0], filters[0])(Concatenate()([x0_0, x0_1, x0_2, UpSampling2D()(x1_2)]))
+
+    x4_0 = conv_block_nested(filters[4], filters[4])(MaxPooling2D(strides=2)(x3_0))
+    x3_1 = conv_block_nested(filters[3], filters[3])(Concatenate()([x3_0, UpSampling2D()(x4_0)]))
+    x2_2 = conv_block_nested(filters[2], filters[2])(Concatenate()([x2_0, x2_1, UpSampling2D()(x3_1)]))
+    x1_3 = conv_block_nested(filters[1], filters[1])(Concatenate()([x1_0, x1_1, x1_2, UpSampling2D()(x2_2)]))
+    x0_4 = conv_block_nested(filters[0], filters[0])(Concatenate()([x0_0, x0_1, x0_2, x0_3, UpSampling2D()(x1_3)]))
+
+    o = Conv2D(nClasses, (3, 3), padding='same')(x0_4)
+
+    outputHeight = Model(inputs, o).output_shape[1]
+    outputWidth = Model(inputs, o).output_shape[2]
+
+    out = (Reshape((outputHeight * outputWidth, nClasses)))(o)
+    out = Activation('softmax')(out)
+
+    model = Model(input=inputs, output=out)
+    model.outputHeight = outputHeight
+    model.outputWidth = outputWidth
+
+    return model
