b2e114895f8120ce45ff952218d6e7afa73f78d4
BBuf <1182563586@qq.com>
2020-05-26 22:33:18 +0800

    Add Unet_Family

--- Models/UNet_Family.py
+++ Models/UNet_Family.py
@@ -179,3 +179,98 @@ def R2UNet(nClasses, input_height=224, input_width=224):
     return model
 
 
+class Attention_block(Model):
+    """
+    Attention Block
+    """
+
+    def __init__(self, filters):
+        super(Attention_block, self).__init__()
+
+        self.W_g = Sequential([
+            Conv2D(filters, kernel_size=1, strides=1, padding='same'),
+            BatchNormalization()
+        ])
+
+        self.W_x = Sequential([
+            Conv2D(filters, kernel_size=1, strides=1, padding='same'),
+            BatchNormalization()
+        ])
+
+        self.psi = Sequential([
+            Conv2D(filters, kernel_size=1, strides=1, padding='same'),
+            BatchNormalization(),
+            Activation('sigmoid')
+        ])
+
+        self.relu = Activation('relu')
+
+    def call(self, x):
+        g1 = self.W_g(x[0])
+        x1 = self.W_x(x[1])
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        out = x[1] * psi
+        return out
+
+def AttUNet(nClasses, input_height=224, input_width=224):
+    """
+    Attention Unet implementation
+    Paper: https://arxiv.org/abs/1804.03999
+    """
+    inputs = Input(shape=(input_height, input_width, 3))
+    n1 = 32
+    filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+
+    e1 = conv_block(filters[0])(inputs)
+
+    e2 = MaxPooling2D(strides=2)(e1)
+    e2 = conv_block(filters[1])(e2)
+
+    e3 = MaxPooling2D(strides=2)(e2)
+    e3 = conv_block(filters[2])(e3)
+
+    e4 = MaxPooling2D(strides=2)(e3)
+    e4 = conv_block(filters[3])(e4)
+
+    e5 = MaxPooling2D(strides=2)(e4)
+    e5 = conv_block(filters[4])(e5)
+
+    d5 = up_conv(filters[3])(e5)
+    x4 =  Attention_block(filters[3])([d5, e4])
+    d5 = Concatenate()([x4, d5])
+    d5 = conv_block(filters[3])(d5)
+
+    d4 = up_conv(filters[2])(d5)
+    x3 =  Attention_block(filters[2])([d4, e3])
+    d4 = Concatenate()([x3, d4])
+    d4 = conv_block(filters[2])(d4)
+
+    d3 = up_conv(filters[1])(d4)
+    x2 =  Attention_block(filters[1])([d3, e2])
+    d3 = Concatenate()([x2, d3])
+    d3 = conv_block(filters[1])(d3)
+
+    d2 = up_conv(filters[0])(d3)
+    x1 =  Attention_block(filters[0])([d2, e1])
+    d2 = Concatenate()([x1, d2])
+    d2 = conv_block(filters[0])(d2)
+
+    o = Conv2D(nClasses, (3, 3), padding='same')(d2)
+
+    outputHeight = Model(inputs, o).output_shape[1]
+    outputWidth = Model(inputs, o).output_shape[2]
+
+    out = (Reshape((outputHeight * outputWidth, nClasses)))(o)
+    out = Activation('softmax')(out)
+
+    model = Model(input=inputs, output=out)
+    model.outputHeight = outputHeight
+    model.outputWidth = outputWidth
+
+    return model
+
+
+
+
+
