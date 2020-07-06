91df612c526ae2240a24e1cb918abfe608eeecd6
张晓雨 <zhangxiaoyu1@rockontrol.com>
2020-07-06 18:34:51 +0800

    Fix BUG

--- Models/R2AttUNet.py
+++ Models/R2AttUNet.py
@@ -4,123 +4,53 @@ import keras
 from keras.models import *
 from keras.layers import *
 
-class conv_block(Model):
-    def __init__(self, filters):
-        super(conv_block, self).__init__()
-        self.output_dim = filters
+def conv_block(input, filters):
+    out = Conv2D(filters, kernel_size=(3,3), strides=1, padding='same')(input)
+    out = BatchNormalization()(out)
+    out = Activation('relu')(out)
+    out = Conv2D(filters, kernel_size=(3,3), strides=1, padding='same')(out)
+    out = BatchNormalization()(out)
+    out = Activation('relu')(out)
+    return out
 
-        self.conv = Sequential([
-            Conv2D(filters, kernel_size=(3,3), strides=1, padding='same'),
-            BatchNormalization(),
-            Activation('relu'),
-            Conv2D(filters, kernel_size=(3,3), strides=1, padding='same'),
-            BatchNormalization(),
-            Activation('relu')
-        ])
+def up_conv(input, filters):
+    out = UpSampling2D()(input)
+    out = Conv2D(filters, kernel_size=(3,3), strides=1, padding='same')(out)
+    out = BatchNormalization()(out)
+    out = Activation('relu')(out)
+    return out
 
-    def call(self, x):
-        x = self.conv(x)
-        return x
-    def compute_output_shape(self, input_shape):
-        space = input_shape[1:-1]
-        return (input_shape[0], space[0], space[1], self.output_dim)
+def Attention_block(input1, input2, filters):
+    g1 = Conv2D(filters, kernel_size=1, strides=1, padding='same')(input1)
+    g1 = BatchNormalization()(g1)
+    x1 = Conv2D(filters, kernel_size=1, strides=1, padding='same')(input2)
+    x1 = BatchNormalization()(x1)
+    psi = Activation('relu')(add([g1, x1]))
+    psi = Conv2D(filters, kernel_size=1, strides=1, padding='same')(psi)
+    psi = BatchNormalization()(psi)
+    psi = Activation('sigmoid')(psi)
+    out = multiply([input2, psi])
+    return out
 
-class up_conv(Model):
-    def __init__(self, filters):
-        super(up_conv, self).__init__()
-        self.output_dim = filters
+def Recurrent_block(input, channel, t=2):
+    for i in range(t):
+        if i == 0:
+            x = Conv2D(channel, kernel_size=(3, 3), strides=1, padding='same')(input)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+        out = Conv2D(channel, kernel_size=(3, 3), strides=1, padding='same')(add([x, x]))
+        out = BatchNormalization()(out)
+        out = Activation('relu')(out)
+    return out
 
-        self.up = Sequential([
-            UpSampling2D(),
-            Conv2D(filters, kernel_size=(3,3), strides=1, padding='same'),
-            BatchNormalization(),
-            Activation('relu')
-        ])
-
-    def call(self, x):
-        x = self.up(x)
-        return x
-    def compute_output_shape(self, input_shape):
-        space = input_shape[1:-1]
-        return (input_shape[0], space[0]*2, space[1]*2, self.output_dim)
-
-class Recurrent_block(Model):
-    def __init__(self, out_ch, t=2):
-        super(Recurrent_block, self).__init__()
-        self.output_dim = out_ch
-        self.t = t
-        self.conv = Sequential([
-            Conv2D(out_ch, kernel_size=(3, 3), strides=1, padding='same'),
-            BatchNormalization(),
-            Activation('relu')
-        ])
-
-    def call(self, x):
-        for i in range(self.t):
-            if i == 0:
-                x = self.conv(x)
-            out = self.conv(x + x)
-        return out
-    def compute_output_shape(self, input_shape):
-        space = input_shape[1:-1]
-        return (input_shape[0], space[0], space[1], self.output_dim)
+def RRCNN_block(input, channel, t=2):
+    x1 = Conv2D(channel, kernel_size=(1, 1), strides=1, padding='same')(input)
+    x2 = Recurrent_block(x1, channel, t=t)
+    x2 = Recurrent_block(x2, channel, t=t)
+    out = add([x1, x2])
+    return out
 
 
-class RRCNN_block(Model):
-    def __init__(self, out_ch, t=2):
-        super(RRCNN_block, self).__init__()
-        self.output_dim = out_ch
-        self.RCNN = Sequential([
-            Recurrent_block(out_ch, t=t),
-            Recurrent_block(out_ch, t=t)
-        ])
-        self.Conv = Conv2D(out_ch, kernel_size=(1, 1), strides=1, padding='same')
-
-    def call(self, x):
-        x1 = self.Conv(x)
-        x2 = self.RCNN(x1)
-        out = x1 + x2
-        return out
-    def compute_output_shape(self, input_shape):
-        space = input_shape[1:-1]
-        return (input_shape[0], space[0], space[1], self.output_dim)
-
-class Attention_block(Model):
-    """
-    Attention Block
-    """
-
-    def __init__(self, filters):
-        super(Attention_block, self).__init__()
-        self.output_dim = filters
-        self.W_g = Sequential([
-            Conv2D(filters, kernel_size=1, strides=1, padding='same'),
-            BatchNormalization()
-        ])
-
-        self.W_x = Sequential([
-            Conv2D(filters, kernel_size=1, strides=1, padding='same'),
-            BatchNormalization()
-        ])
-
-        self.psi = Sequential([
-            Conv2D(filters, kernel_size=1, strides=1, padding='same'),
-            BatchNormalization(),
-            Activation('sigmoid')
-        ])
-
-        self.relu = Activation('relu')
-
-    def call(self, x):
-        g1 = self.W_g(x[0])
-        x1 = self.W_x(x[1])
-        psi = self.relu(g1 + x1)
-        psi = self.psi(psi)
-        out = x[1] * psi
-        return out
-    def compute_output_shape(self, input_shape):
-        space = input_shape[1:-1]
-        return (input_shape[0], space[0]*2, space[1]*2, self.output_dim)
 
 def R2AttUNet(nClasses, input_height=224, input_width=224):
     # """
@@ -132,39 +62,39 @@ def R2AttUNet(nClasses, input_height=224, input_width=224):
     n1 = 32
     filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
 
-    e1 = RRCNN_block(filters[0], t=t)(inputs)
+    e1 = RRCNN_block(inputs, filters[0], t=t)
 
     e2 = MaxPooling2D(strides=2)(e1)
-    e2 = RRCNN_block(filters[1], t=t)(e2)
+    e2 = RRCNN_block(e2, filters[1], t=t)
 
     e3 = MaxPooling2D(strides=2)(e2)
-    e3 = RRCNN_block(filters[2], t=t)(e3)
+    e3 = RRCNN_block(e3, filters[2], t=t)
 
     e4 = MaxPooling2D(strides=2)(e3)
-    e4 = RRCNN_block(filters[3], t=t)(e4)
+    e4 = RRCNN_block(e4, filters[3], t=t)
 
     e5 = MaxPooling2D(strides=2)(e4)
-    e5 = RRCNN_block(filters[4], t=t)(e5)
+    e5 = RRCNN_block(e5, filters[4], t=t)
 
-    d5 = up_conv(filters[3])(e5)
-    x4 =  Attention_block(filters[3])([d5, e4])
+    d5 = up_conv(e5, filters[3])
+    x4 =  Attention_block(d5, e4, filters[3])
     d5 = Concatenate()([x4, d5])
-    d5 = conv_block(filters[3])(d5)
+    d5 = conv_block(d5, filters[3])
 
-    d4 = up_conv(filters[2])(d5)
-    x3 =  Attention_block(filters[2])([d4, e3])
+    d4 = up_conv(d5, filters[2])
+    x3 =  Attention_block(d4, e3, filters[2])
     d4 = Concatenate()([x3, d4])
-    d4 = conv_block(filters[2])(d4)
+    d4 = conv_block(d4, filters[2])
 
-    d3 = up_conv(filters[1])(d4)
-    x2 =  Attention_block(filters[1])([d3, e2])
+    d3 = up_conv(d4, filters[1])
+    x2 =  Attention_block(d3, e2, filters[1])
     d3 = Concatenate()([x2, d3])
-    d3 = conv_block(filters[1])(d3)
+    d3 = conv_block(d3, filters[1])
 
-    d2 = up_conv(filters[0])(d3)
-    x1 =  Attention_block(filters[0])([d2, e1])
+    d2 = up_conv(d3, filters[0])
+    x1 =  Attention_block(d2, e1, filters[0])
     d2 = Concatenate()([x1, d2])
-    d2 = conv_block(filters[0])(d2)
+    d2 = conv_block(d2, filters[0])
 
     o = Conv2D(nClasses, (3, 3), padding='same')(d2)
 
