afe93137ef777591ba9df67595e491c9146b7309
Zichen Liu <zichen.liu@shopee.com>
2020-07-11 21:00:17 +0800

    save weight only

--- runs/default.sh
+++ runs/default.sh
@@ -1,6 +1,11 @@
-export CUDA_VISIBLE_DEVICES=0
-python train.py --model_name vggunet 
-python train.py --model_name fcn8 
+export CUDA_VISIBLE_DEVICES=1
+python train.py --model_name unet --exp_name ce --loss ce
+python train.py --model_name enet --exp_name w_ce --loss weighted_ce
+python train.py --model_name enet --exp_name b_focal --loss b_focal ~!!
+python train.py --model_name enet --exp_name c_focal --loss c_focal ~!!
+python train.py --model_name enet --exp_name dice --loss dice ~!!
+
+
 
 export CUDA_VISIBLE_DEVICES=1
 python train.py --model_name unet_xception_resnetblock
--- train.py
+++ train.py
@@ -139,7 +139,7 @@ model_names = os.path.join(train_save_path, '%s.{epoch:02d}-{acc:2f}.hdf5' % (
 model_checkpoint = ModelCheckpoint(model_names,
                                    monitor='val_iou_score',
                                    save_best_only=True,
-                                   save_weights_only=False)
+                                   save_weights_only=True)
 
 # if multi_gpus == True:
 #     model_checkpoint = ParallelModelCheckpoint(model, filepath=model_names,
